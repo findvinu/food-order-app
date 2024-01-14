0da6ea49fdc869c0cfa4d5da72cff08f841f71b3
findvinu <vinodyadavhhh4@gmail.com>
2024-01-14 18:28:36 +0530

    card update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,16 +3,17 @@ import mealsImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = props => {
-    return ( 
-        <>
+    return (
+      <>
         <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCartButton onShowCart={props.onShowCart} />
+          <h1>Meals</h1>
+          <HeaderCartButton onShowCart={props.onShowCart} />
         </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImg} alt='meals' />
+        <div className={classes["main-image"]}>
+          <img src={mealsImg} alt="meals" />
         </div>
-    </>);
+      </>
+    );
 }
  
 export default Header;
